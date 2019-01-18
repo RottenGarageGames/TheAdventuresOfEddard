6b78912bf1fa305078722060af4962ca56ac3cfd
michaelpeoples <42450795+michaelpeoples@users.noreply.github.com>
2019-01-17 22:54:08 -0600

    Lamp Post Code Ceiling Spikes

---
+++ 2D SideScroller/Assets/StalactiteMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalactiteMovement : MonoBehaviour
+{
+     private Rigidbody2D rigidBody;
+     public float fallSpeed;
+     public int damageAmount;
+     public string startCollider;
+     
+     private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            rigidBody.gravityScale = fallSpeed;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        //other.SendMessage("TakeDamage", damageAmount);
+        if (other.gameObject.tag != startCollider)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    
+}
