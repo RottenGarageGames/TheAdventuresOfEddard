30f10dd73ce134836731ce1f136f8a9da725f9f4
Matt <mattppls4@gmail.com>
2019-03-09 20:20:40 -0600

    Light/WallWalking

---
+++ 2D SideScroller/Assets/EyeMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeMovement : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetBool("playerEnter", true);
+
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+
+        }
+    }
+}
