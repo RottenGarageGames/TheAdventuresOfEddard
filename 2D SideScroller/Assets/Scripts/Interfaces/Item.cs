95b70f69929cfc8fd90df28708e9936d6af58a8e
michaelpeoples <42450795+michaelpeoples@users.noreply.github.com>
2019-03-22 20:58:57 -0500

    Reimport Animations - Made new Test scene

--- 2D SideScroller/Assets/Scripts/Interfaces/Item.cs
+++ 2D SideScroller/Assets/Scripts/Interfaces/Item.cs
@@ -6,16 +6,25 @@ using System;
 
 namespace Items
 {
-    public class Item : MonoBehaviour, IInventoryItem
+    public class Item : MonoBehaviour, IInventoryItem, IInteractable
     {
         [SerializeField]
         private String _name;
         public String name { get { return _name; } set { _name = value; _name = value; } }
+
         [SerializeField]
         private int _itemID;
         public int itemID { get { return _itemID; } set { _itemID = value; itemID = value; } }
 
+        [SerializeField]
+        private float _interactRadius;
+        public float interactRadius { get { return _interactRadius; } set { _interactRadius = value; interactRadius = value; } }
+
         public Item()
+        {
+
+        }
+        public void Interact()
         {
 
         }
