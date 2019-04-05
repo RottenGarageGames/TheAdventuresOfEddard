34de1b4b019b7579020c4979656016f729744b68
michaelpeoples <42450795+michaelpeoples@users.noreply.github.com>
2019-04-05 00:12:47 -0500

    Fixed Bug with Player Health

---
+++ 2D SideScroller/Assets/Scripts/Items/MaxShieldPotion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityInterfaces;
+
+public class MaxShieldPotion : StackableItem, IConsumable
+{
+    public void Consume(GameObject player)
+    {
+        var healthComponent = player.GetComponent<PlayerHealth>();
+        healthComponent.MaxShield();
+    }
+}
--- 2D SideScroller/Assets/Scripts/PlayerHealth.cs
+++ 2D SideScroller/Assets/Scripts/PlayerHealth.cs
@@ -1,42 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public Sprite healthIcon;
+    public Sprite shieldIcon;
+
     public int playerHealth;
+    public int shieldHealth;
     public int maxHealth;
+    public int maxShield;
     public List<GameObject> healthIcons;
     public Canvas playerCanvas;
     // Start is called before the first frame update
     void Start()
     {
         playerCanvas = gameObject.GetComponent<Canvas>();
-        maxHealth = playerHealth;
+        playerHealth = maxHealth;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerHealth <= 0)
+        if (playerHealth <= 0)
         {
-            Destroy(gameObject);
+          Destroy(gameObject);
         }
     }
     public void TakeDamage(int damage)
     {
-        playerHealth -= damage;
+        //Calculate the leftover damage to apply to health
+        var leftoverDamage = damage - shieldHealth;
 
-        //Could also call change sprite here instead if we'd rather replace with another colored sprite
-        healthIcons[playerHealth].SetActive(false);
+        //Damage Shields first
+        if (shieldHealth > 0)
+        {
+            shieldHealth -= damage;
+        }
+        //If shield doesn't cover damage apply to health
+        if(leftoverDamage > 0)
+        {
+            playerHealth -= damage;
+        }
+
+        UpdateIcons();
     }
     public void RestoreHealth(int health)
     {
         if (playerHealth < maxHealth)
         {
             playerHealth += health;
-            
+
             foreach (GameObject healthIcon in healthIcons)
             {
                 if (!healthIcon.activeSelf)
@@ -52,9 +69,43 @@ public class PlayerHealth : MonoBehaviour
     {
         playerHealth = maxHealth;
 
-        foreach(GameObject healthIcon in healthIcons)
+        foreach (GameObject healthIcon in healthIcons)
         {
             healthIcon.SetActive(true);
+        }
+    }
+    public void AddShield(int shieldAmount)
+    {
+        shieldHealth += shieldAmount;
+        UpdateIcons();
+    }
+    public void MaxShield()
+    {
+        shieldHealth = maxShield;
+        UpdateIcons();
+    }
+    public void UpdateIcons()
+    {
+        var count = 1;
+
+        foreach (GameObject slot in healthIcons)
+        {
+
+
+            if(count <= shieldHealth)
+            {
+                slot.GetComponent<Image>().sprite = shieldIcon;
+            }
+            else if(count <= playerHealth)
+            {
+                slot.GetComponent<Image>().sprite = healthIcon;
+            }
+            else
+            {
+               slot.SetActive(false);
+            }
+
+            count++;
         }
     }
 }
