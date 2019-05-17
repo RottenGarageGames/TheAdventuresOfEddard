31f1b9622877419e736ec91f2c6f448f7f1a68c6
CalebPowell57 <45093056+CalebPowell57@users.noreply.github.com>
2019-05-16 23:23:25 -0500

    Changed player controller

---
+++ 2D SideScroller/Assets/Scripts/CalebPlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CalebPlayerController : MonoBehaviour
+{
+    public enum PlayerDirection { Left, Right };
+
+    public Rigidbody2D RigidBody { get; private set; }
+
+    public float WalkSpeed = 10;
+    public float RunSpeed = 18;
+    public float JumpForce = 10;
+
+    public PlayerDirection Direction { get; private set; }
+    
+    public bool Running { get; set; }
+    public bool IsGrounded { get; private set; }
+    public Transform GroundCheck;
+    public LayerMask Ground;
+    public float PlayerCurrentFlipValue;
+
+    void Start()
+    {
+        RigidBody = GetComponent<Rigidbody2D>();
+        PlayerCurrentFlipValue = transform.rotation.y;
+    }
+    
+    void FixedUpdate()
+    {
+        IsGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.3f, Ground);
+    }
+
+    public void HorizontalMove(float horizontalInput)
+    {
+        if (horizontalInput != 0)
+        {
+            UpdateDirection(horizontalInput > 0 ? PlayerDirection.Right : PlayerDirection.Left);
+        }
+
+        var speed = Running ? RunSpeed : WalkSpeed;
+
+        var x = horizontalInput * 2 * speed;
+
+        RigidBody.velocity = new Vector2(x, RigidBody.velocity.y);
+    }
+
+    public void Jump(float input)
+    {
+        if (IsGrounded)
+        {
+            var y = input * JumpForce * 4;
+
+            RigidBody.velocity = new Vector2(RigidBody.velocity.x, y);
+        }
+    }
+
+    private void UpdateDirection(PlayerDirection playerDirection)
+    {
+        if(Direction == playerDirection)
+        {
+            return;
+        }
+
+        Direction = playerDirection;
+
+        Vector3 newScale = transform.localScale;
+        newScale.x *= -1;
+        transform.localScale = newScale;
+    }
+}
---
+++ 2D SideScroller/Assets/Scripts/Input/GlobalInputManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalInputManager : MonoBehaviour
+{
+    public enum InputAction { Jump, Run, Interact, InventoryWheel, Attack1, Attack2, Ability1, Ability2 }
+    public enum InputButton { LeftBumper, RightBumper, RightTrigger, LeftTrigger, LeftThumbButton, BottomThumbButton, RightThumbButton, TopThumbButton }
+
+    public Animator DialogueAnimator;
+    public DialogueManager DialogueManager;
+    public CalebPlayerController PlayerController;
+
+    private float _lastJumpInput = 0;
+
+    private float _bottomThumbButtonInput = 0;
+    private float _topThumbButtonInput = 0;
+    private float _leftThumbButtonInput = 0;
+    private float _rightThumbButtonInput = 0;
+
+    private Dictionary<InputAction, InputButton> Bindings = new Dictionary<InputAction, InputButton>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bindings.Add(InputAction.Jump, InputButton.BottomThumbButton);
+        Bindings.Add(InputAction.Interact, InputButton.LeftThumbButton);
+        Bindings.Add(InputAction.InventoryWheel, InputButton.RightThumbButton);
+        Bindings.Add(InputAction.Run, InputButton.TopThumbButton);
+        Bindings.Add(InputAction.Ability1, InputButton.LeftBumper);
+        Bindings.Add(InputAction.Attack1, InputButton.LeftTrigger);
+        Bindings.Add(InputAction.Ability2, InputButton.RightBumper);
+        Bindings.Add(InputAction.Attack2, InputButton.RightTrigger);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        _topThumbButtonInput = Input.GetAxis("TopThumbButton");
+        _leftThumbButtonInput = Input.GetAxis("LeftThumbButton");
+        _bottomThumbButtonInput = Input.GetAxis("BottomThumbButton");
+        _rightThumbButtonInput = Input.GetAxis("RightThumbButton");
+
+        var jumpInput = GetInput(InputAction.Jump);
+        var interactInput = GetInput(InputAction.Interact);
+        var inventoryInput = GetInput(InputAction.InventoryWheel);
+        var runInput = GetInput(InputAction.Run);
+
+        if (_leftThumbButtonInput != 0)
+        {
+            if(DialogueAnimator.GetBool("IsOpen"))
+            {
+                DialogueManager.DisplayNextSentence();
+            }
+            else
+            {
+                //PlayerController?.Interact();
+            }
+        }
+
+        PlayerController.Running = runInput > 0;
+        PlayerController.HorizontalMove(horizontalInput);
+
+        if (jumpInput != 0 && _lastJumpInput == 0)
+        {
+            PlayerController.Jump(jumpInput);
+        }
+
+        _lastJumpInput = jumpInput;
+    }
+
+    private float GetInput(InputAction inputAction)
+    {
+        var button = Bindings[inputAction];
+
+        switch(button)
+        {
+            case InputButton.BottomThumbButton: return _bottomThumbButtonInput;
+            case InputButton.TopThumbButton: return _topThumbButtonInput;
+            case InputButton.RightThumbButton: return _rightThumbButtonInput;
+            case InputButton.LeftThumbButton: return _leftThumbButtonInput;
+            default: return 0;
+        }
+    }
+}
