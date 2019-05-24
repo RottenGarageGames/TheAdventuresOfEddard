39025ff098d2ca6db1035d93b59a6d7f0db3bec1
michaelpeoples <42450795+michaelpeoples@users.noreply.github.com>
2019-05-23 20:59:11 -0500

    Interact

---
+++ 2D SideScroller/Assets/Coin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityInterfaces;
+
+public class Coin : Stat, IInteractable
+{
+    [SerializeField]
+    private float _interactRadius;
+    public float interactRadius { get; set; }
+
+    public void Interact(GameObject gameObject)
+    {
+        var purse = gameObject.GetComponent<Currency>();
+
+        if(purse != null)
+        {
+            purse.IncreaseStat(stat);
+        }
+    }
+}
